93d2b5163178262411ed5fe6072e29fb043e5ed1
xiaoj <xiaojgz@163.com>
2023-09-26 08:52:23 +0800

    UR5读数读出的角度是旋转矢量

--- caculate_matrix/tool.py
+++ caculate_matrix/tool.py
@@ -7,6 +7,8 @@
 """
 import numpy as np
 import math
+from math import sin
+from math import cos
 from scipy.spatial.transform import Rotation as R
 np.set_printoptions(suppress=True)
 
@@ -57,22 +59,41 @@ def rotationMatrixToEulerAngles(R):
 
     return np.array([x, y, z])
 
-def getRotationAndTransferMatrix(postion):
-    r_matrix = eulerAnglesToRotationMatrix(postion[3:6])
-    r_t_matrix = np.c_[np.r_[r_matrix,np.array([[0, 0, 0]], dtype=float)], np.array([[postion[0],postion[1],
-                                                                                     postion[2], 1.0]], dtype=float).T]
-    return r_t_matrix
+
 
 def euler2rot(euler):
-    r = R.from_euler('xyz', euler, degrees=False)
+    r = R.from_euler('zyx', euler, degrees=False)
     rotation_matrix = r.as_matrix()
     return rotation_matrix
 
 def eor2euler(matrix):
     r = R.from_matrix(matrix)
-    euler = r.as_euler('xyz', degrees=False)
+    euler = r.as_euler('zyx', degrees=False)
     return euler
 
+
+
+def rodrigues(rot_vector):
+    """"""
+
+    theta = np.linalg.norm(rot_vector)
+    rot_vector = np.array(rot_vector).reshape(3, 1) / theta
+
+    K = np.asanyarray(
+        [
+            [0, -rot_vector[2, 0], rot_vector[1, 0]],
+            [rot_vector[2, 0], 0, -rot_vector[0, 0]],
+            [-rot_vector[1, 0], rot_vector[0, 0], 0]
+        ]
+    )
+
+    return np.asanyarray(np.cos(theta)*np.eye(3) + (1 - np.cos(theta))*rot_vector*rot_vector.T + np.sin(theta) * K )
+
+def getRotationAndTransferMatrix(postion):
+    r_matrix = rodrigues(postion[3:6])
+    r_t_matrix = np.c_[np.r_[r_matrix,np.array([[0, 0, 0]], dtype=float)], np.array([[postion[0],postion[1],
+                                                                                     postion[2], 1.0]], dtype=float).T]
+    return r_t_matrix
 # print(euler2rot(np.array([-3.07759265, -0.58259265, -1.36759265])))
 # print(euler2rot(np.array([0.064,-2.559,1.774])))
 #
@@ -84,9 +105,10 @@ shift_matrix = np.array([[1,0,0,0],
                          [0,0,1,0.2],
                          [0,0,0,1]])
 m = getRotationAndTransferMatrix(np.array([-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 ]))
+# print(m)
 r = m.dot(shift_matrix)
-print(m)
 print(r)
+
 
 '''
 x + 100
@@ -98,4 +120,16 @@ z + 200
 合
 -181.23，-506.65，408.17
 '''
+np.array([-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 ])
+px = -0.08257
+py = -0.32168
+pz = 0.53478
+r = 0.126
+y = -2.392
+p = 2.01
+ttt = np.array([[cos(r)*cos(y), cos(r)*sin(p)*sin(y) - cos(p)*sin(r), sin(p)*sin(r) + cos(p)*cos(r)*sin(y),px],
+                [cos(y)*sin(r), cos(p)*cos(r) + sin(r)*sin(y)*sin(p), cos(p)*sin(y)*sin(r) - cos(r)*sin(r), py],
+                [-sin(y),cos(y)*sin(p),cos(p)*cos(y), pz],
+                [0,0,0,1]])
+
 
