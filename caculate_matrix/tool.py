4296e58e71e7353ffe4fe462144c514958b2b2e8
xiaoj <xiaojgz@163.com>
2023-09-26 10:44:40 +0800

    增加旋转矢量和旋转矩阵之前的相互转换

--- caculate_matrix/tool.py
+++ caculate_matrix/tool.py
@@ -7,9 +7,7 @@
 """
 import numpy as np
 import math
-from math import sin
-from math import cos
-from scipy.spatial.transform import Rotation as R
+import cv2 as cv
 np.set_printoptions(suppress=True)
 
 f_carmera = 910.0
@@ -21,8 +19,12 @@ def getCameraPosion(xl, yl, xr, yr):
     return x,y,z
 
 
-# Calculates Rotation Matrix given euler angles.
 def eulerAnglesToRotationMatrix(theta):
+    '''
+    欧拉角（弧度）到旋转矩阵
+    :param theta:
+    :return:
+    '''
     R_x = np.array([[1, 0, 0],
                     [0, math.cos(theta[0]), -math.sin(theta[0])],
                     [0, math.sin(theta[0]), math.cos(theta[0])]])
@@ -43,7 +45,11 @@ def eulerAnglesToRotationMatrix(theta):
 
 
 def rotationMatrixToEulerAngles(R):
-
+    '''
+    旋转矩阵转换欧拉角（弧度）
+    :param R:
+    :return:
+    '''
     sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
 
     singular = sy < 1e-6
@@ -60,25 +66,14 @@ def rotationMatrixToEulerAngles(R):
     return np.array([x, y, z])
 
 
-
-def euler2rot(euler):
-    r = R.from_euler('zyx', euler, degrees=False)
-    rotation_matrix = r.as_matrix()
-    return rotation_matrix
-
-def eor2euler(matrix):
-    r = R.from_matrix(matrix)
-    euler = r.as_euler('zyx', degrees=False)
-    return euler
-
-
-
-def rodrigues(rot_vector):
-    """"""
-
+def rotateVector2Matrix(rot_vector):
+    '''
+    旋转矢量转化为旋转矩阵
+    :param rot_vector:
+    :return:
+    '''
     theta = np.linalg.norm(rot_vector)
     rot_vector = np.array(rot_vector).reshape(3, 1) / theta
-
     K = np.asanyarray(
         [
             [0, -rot_vector[2, 0], rot_vector[1, 0]],
@@ -86,31 +81,20 @@ def rodrigues(rot_vector):
             [-rot_vector[1, 0], rot_vector[0, 0], 0]
         ]
     )
-
     return np.asanyarray(np.cos(theta)*np.eye(3) + (1 - np.cos(theta))*rot_vector*rot_vector.T + np.sin(theta) * K )
 
+def matrxi2RotateVector(matrix):
+    vec = cv.Rodrigues(matrix)[0]
+    return vec
 def getRotationAndTransferMatrix(postion):
-    r_matrix = rodrigues(postion[3:6])
+    r_matrix = rotateVector2Matrix(postion[3:6])
     r_t_matrix = np.c_[np.r_[r_matrix,np.array([[0, 0, 0]], dtype=float)], np.array([[postion[0],postion[1],
                                                                                      postion[2], 1.0]], dtype=float).T]
     return r_t_matrix
-# print(euler2rot(np.array([-3.07759265, -0.58259265, -1.36759265])))
-# print(euler2rot(np.array([0.064,-2.559,1.774])))
-#
-# print(eor2euler(euler2rot(np.array([0.064,-2.559,1.774]))))
-# print(rotationMatrixToEulerAngles(euler2rot(np.array([0.064,-2.559,1.774]))))
 
-shift_matrix = np.array([[1,0,0,0],
-                         [0,1,0,0],
-                         [0,0,1,0.2],
-                         [0,0,0,1]])
-m = getRotationAndTransferMatrix(np.array([-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 ]))
-# print(m)
-r = m.dot(shift_matrix)
-print(r)
-
-
+m = rotateVector2Matrix(np.array([-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 ])[3:6])
 '''
+-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 
 x + 100
 -182.2,-326.91,541.06
 Y + 100
@@ -120,16 +104,6 @@ z + 200
 合
 -181.23，-506.65，408.17
 '''
-np.array([-0.08257,-0.32168,0.53478,0.126,-2.392,2.01 ])
-px = -0.08257
-py = -0.32168
-pz = 0.53478
-r = 0.126
-y = -2.392
-p = 2.01
-ttt = np.array([[cos(r)*cos(y), cos(r)*sin(p)*sin(y) - cos(p)*sin(r), sin(p)*sin(r) + cos(p)*cos(r)*sin(y),px],
-                [cos(y)*sin(r), cos(p)*cos(r) + sin(r)*sin(y)*sin(p), cos(p)*sin(y)*sin(r) - cos(r)*sin(r), py],
-                [-sin(y),cos(y)*sin(p),cos(p)*cos(y), pz],
-                [0,0,0,1]])
+
 
 
