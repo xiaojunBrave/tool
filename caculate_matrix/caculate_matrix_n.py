2763bb7831b00070ee3c4ee18a9292f8fa81a4fc
xiaoj <xiaojgz@163.com>
2023-10-16 16:28:58 +0800

    修改新的矩阵计算方法

--- caculate_matrix/caculate_matrix_n.py
+++ caculate_matrix/caculate_matrix_n.py
@@ -10,54 +10,54 @@ import data as dt
 from numpy.linalg import inv
 np.set_printoptions(suppress=True)
 # data
+# select_data_index = 2
+def getA_B(select_data_index):
+    camera_left = dt.camera_lefts[select_data_index].copy()
+    camera_right = dt.camera_rights[select_data_index].copy()
+    tcp_target_modify = dt.tcp_target_modifys[select_data_index].copy()
+    tcp_base = dt.tcp_bases[select_data_index].copy()
 
-tcp_base = dt.tcp_base.copy()
-tcp_target_modify = dt.tcp_target_modify.copy()
-camera_left = dt.camera_left.copy()
-camera_right = dt.camera_right.copy()
+    tcp_base_r_t_matrix = tl.getRotationAndTransferMatrix(tcp_base)
+    tcp_base_r_t_matrix_inv = inv(tcp_base_r_t_matrix)
 
-tcp_base_r_t_matrix = tl.getRotationAndTransferMatrix(tcp_base)
-tcp_base_r_t_matrix_inv = inv(tcp_base_r_t_matrix)
+    # get tcp position
+    tcp_targets = np.zeros((tcp_target_modify.shape[0], 6), dtype=float)
+    for i in range(tcp_target_modify.shape[0]):
+        matrix_base = tl.getRotationAndTransferMatrix(tcp_target_modify[i, :])
+        true_base = matrix_base.dot(dt.move_brush)
+        # print(matrix_base)
+        matrix_tcp = tcp_base_r_t_matrix_inv.dot(true_base)
+        tcp_targets[i:i + 1, 0:3] = matrix_tcp[0:3, 3:4].T
 
-# get tcp position
-tcp_targets = np.zeros((tcp_target_modify.shape[0],6),dtype=float)
-for i in range(tcp_target_modify.shape[0]):
-    matrix_base = tl.getRotationAndTransferMatrix(tcp_target_modify[i, :])
-    true_base = matrix_base.dot(dt.move_brush)
-    # print(matrix_base)
-    matrix_tcp = tcp_base_r_t_matrix_inv.dot(true_base)
-    tcp_targets[i:i+1, 0:3] = matrix_tcp[0:3, 3:4].T
+    # print(tcp_targets)
+    # get camera position
+    camera_positions = np.ones((tcp_targets.shape[0], 3), dtype=float)
+    for i in range(tcp_targets.shape[0]):
+        x, y, z = tl.getCameraPosion(camera_left[i, 0], camera_left[i, 1], camera_right[i, 0], camera_right[i, 0])
+        camera_positions[[i], :] = np.array([[x, y, z]])
 
+    tcp_deta = tcp_targets[:, 0:3]
+    camera_targets = camera_positions
+    # caculate
+    tcp_targets_argument = np.c_[tcp_deta, np.ones((tcp_deta.shape[0], 1), dtype=float)]
+    camera_targets_argument = np.c_[camera_targets, np.ones((camera_targets.shape[0], 1), dtype=float)]
 
-# print(tcp_targets)
-# get camera position
-camera_positions = np.ones((tcp_targets.shape[0], 3), dtype=float)
-for i in range(tcp_targets.shape[0]):
-    x,y,z = tl.getCameraPosion(camera_left[i, 0], camera_left[i, 1], camera_right[i, 0], camera_right[i, 0])
-    camera_positions[[i], :] = np.array([[x, y, z]])
+    # print(tcp_targets_argument)
+    # print(camera_targets_argument)
+    # solve B=AX A:4x4 X:4x4 B:4x4
+    # 从实验数据中选取4组合适的数据
 
-tcp_deta = tcp_targets[:, 0:3]
-camera_targets = camera_positions
-# caculate
-tcp_targets_argument = np.c_[tcp_deta,np.ones((tcp_deta.shape[0],1),dtype=float)]
-camera_targets_argument = np.c_[camera_targets, np.ones((camera_targets.shape[0], 1), dtype=float)]
+    # data_0
+    # selected_row = [0,2,4,5]
 
-# print(tcp_targets_argument)
-# print(camera_targets_argument)
-# solve B=AX A:4x4 X:4x4 B:4x4
-# 从实验数据中选取4组合适的数据
-
-# data_0
-# selected_row = [0,2,4,5]
-
-# data_5
-selected_row = [0,1,2,3]
-
-A = sp.Matrix(camera_targets_argument[selected_row, :])
-B = sp.Matrix(tcp_targets_argument[selected_row,:])
-X = A.solve(B)
-# print(X.T)
-
+    # data_5
+    # selected_row = [0, 1, 2, 3]
+    #
+    # A = sp.Matrix(camera_targets_argument[selected_row, :])
+    # B = sp.Matrix(tcp_targets_argument[selected_row, :])
+    # X = A.solve(B)
+    # print(X.T)
+    return camera_targets_argument,tcp_targets_argument
 
 '''
 [1.00951526488775, 0.00284634828765536, -0.690249743101517, -40.4702041754239],
---
+++ caculate_matrix/caculate_matrix_new.py
@@ -0,0 +1,7 @@
+"""
+@Project :tool
+@File    :caculate_matrix_new.py
+@IDE     :PyCharm
+@Author  :xiaoj
+@Date    :2023/10/16 16:13
+"""
